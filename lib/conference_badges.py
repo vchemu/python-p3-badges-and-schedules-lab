fea7fa0c07ce927a75434d1837486e2639d6e517
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-06 22:10:49 +0300

    loops

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,25 @@
 def badge_maker(name):
-    return None
+    result = f"Hello, my name is {name}."
+    return result
 
 def batch_badge_creator(names):
-    return None
+    speaker_badges = []
+    for name in names:
+        speaker_badges.append(f"Hello, my name is {name}.") 
+    return speaker_badges
 
 def assign_rooms(names):
-    return None
+    speaker_rooms = []
+    for i, name in enumerate(names, start=1):
+        speaker_rooms.append(f"Hello, {name}! You'll be assigned to room {i}!")
+
+    return speaker_rooms
 
 def printer(names):
-    return None
+    speaker_badges = batch_badge_creator(names)
+    for result in speaker_badges:
+        print(result)
+
+    speaker_rooms = assign_rooms(names)
+    for room in speaker_rooms:
+        print(room)
